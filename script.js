9ba8d270dc0d7d231ce8a6becfbb4545eb16710e
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-08-01 15:30:02 -0400

    Create operator function
    One function to pick between all of the basic calculator operators.

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+let firstNum;
+let operator;
+let secondNum;
+
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function subtract(x, y) {
-    return x - y;
+  return x - y;
 }
 
 function multiply(x, y) {
-    return x * y;
+  return x * y;
 }
 
 function divide(x, y) {
-    return  x / y;
+  return x / y;
 }
 
+function operate(firstNum, operator, secondNum) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(firstNum, secondNum);
+      break;
+    case "-":
+      result = subtract(firstNum, secondNum);
+      break;
+    case "*":
+      result = multiply(firstNum, secondNum);
+      break;
+    case "/":
+      result = divide(firstNum, secondNum);
+      break;
+    default:
+        console.log('huh');
+  }
+  return result;
+}
